62ae3e27a2c83ed178e50b5d517a825de41927c7
italeenamk <italeenamk@gmail.com>
2023-06-07 19:15:43 +0500

    navbar content design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "../layout/navbar/Navbar";
 
 
 const Home = () => {
   return(
-      <>home
+      <>
+          <Navbar/>
       </>
   )
 }
---
+++ src/pages/layout/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import './navbar.css';
+
+
+const Navbar = () => {
+  return(
+      <>
+          <section className="top-navbar-content">
+              <div className="container-fluid">
+                  <div className="row">
+                      <div className="col-md-3 col-sm-12">
+                          <div className="top-navbar-logo-content">
+                              <a href="">Kelly</a>
+                          </div>
+                      </div>
+                      <div className="col-md-6 col-sm-12">
+                          <div className="top-navbar-list-content">
+                              <ul>
+                                  <li><a href="">Home</a></li>
+                                  <li><a href="">About</a></li>
+                                  <li><a href="">Resume</a></li>
+                                  <li><a href="">Services</a></li>
+                                  <li><a href="">Portfolio</a></li>
+                                  <li><a href="">Contact</a></li>
+                              </ul>
+                          </div>
+                      </div>
+                      <div className="col-md-3 col-sm-12"></div>
+                  </div>
+              </div>
+          </section>
+      </>
+  )
+}
+
+export default Navbar;
