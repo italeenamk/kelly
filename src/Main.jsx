7dd48e8bebf040e2195e53dd7400ea7ba39beb12
italeenamk <italeenamk@gmail.com>
2023-06-07 15:31:34 +0500

    add bootstrap 4

---
+++ src/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Main = () => {
+  return(
+      <>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+          <h1>hello</h1>
+      </>
+  )
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Main from "./Main";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Main/>
   </React.StrictMode>
 );
 
